64011c9bfdc2288a813316da0df5b9ec97893ad2
KirtiMahale98 <158018712+KirtiMahale98@users.noreply.github.com>
2024-02-01 18:25:54 +0530

    Delete eureka-client directory

--- eureka-client/src/main/java/com/example/eurekaclient/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eurekaclient.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.eurekaclient.entity.Users;
-
-public interface UserRepository extends MongoRepository<Users,String>{ 
-	  
-}
